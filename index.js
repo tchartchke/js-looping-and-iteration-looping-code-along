112d9a70937a5410c0cdda9d33b529ef4d7b77b3
tchartchke <chartai.kl@gmail.com>
2021-04-01 17:21:48 -0500

    done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array, msg){
+  let all = [];
+
+  for (let i = 0; i < array.length; i++) {
+    all.push(`Thank you, ${array[i]}, for the wonderful ${msg} gift!`)
+  }
+
+  return all
+
+}
+
+function countDown(num){
+  while (num >= 0){
+    console.log(num--);
+    
+  }
+}
